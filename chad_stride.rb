dba5a518a6333f62e33a321932f581d7cb3a99d0
sambattalio <sambattalio99@gmail.com>
2020-02-13 17:11:31 -0500

    base formulae

---
+++ chad_stride.rb
@@ -0,0 +1,15 @@
+class ChadStride < Formula
+  desc "A small curses program to stride across your terminal"
+  homepage ""
+  url "https://github.com/sambattalio/chad_stride/archive/0.69.tar.gz"
+  sha256 "4b5541728629568d3d7078391b1944e02b13a62975641faa50bf814bc63df0f8"
+
+  def install
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/cs"
+  end
+end
