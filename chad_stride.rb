9a788a65c554de4c76f8701fad3fb3e8cc1f1a8c
sambattalio <sambattalio99@gmail.com>
2020-10-02 12:16:31 -0400

    update chad_stride

--- chad_stride.rb
+++ chad_stride.rb
@@ -1,13 +1,14 @@
 class ChadStride < Formula
   desc "A small curses program to stride across your terminal"
   homepage ""
-  url "https://github.com/sambattalio/chad_stride/archive/0.69420.tar.gz"
-  sha256 "7b8995f2cf75a4211eae61dcee2e2691b8a68cc2ed573e35c798672d33a30432"
+  url "https://github.com/sambattalio/chad_stride/archive/0.6951.tar.gz"
+  sha256 "71b44c8143b87abded997b6d77332dfa81e895f822eab025bb2f6fc5a2ca8caa"
 
   def install
     system "make"
     bin.install "chad_stride"
     system "ln", "-s", "#{bin}/chad_stride", "#{bin}/cs"
+    man1.install "chad_stride.1"
   end
 
   test do
